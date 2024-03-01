4ad6736946d41bf064822229fd7a35528d0a9e7a
Sufiyan Shaikh <sufiwork2001@gmail.com>
2024-03-01 09:36:49 +0530

    :fire: maxchar solved

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,27 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charMap = {};
+
+  for (let char of str) {
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+  }
+
+  let maxChar = "";
+
+  for (let char in charMap) {
+    if (charMap[char] > maxChar) {
+      maxChar = char;
+      maxCount = charMap[char];
+    }
+  }
+
+  return maxChar;
+}
 
 module.exports = maxChar;
